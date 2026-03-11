3356f031d3c2b6e40810bf0ab907d20c3534d227
unknown <diannycast@gmail.com>
2026-03-11 06:56:01 -0500

    invertirArreglo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -141,9 +141,11 @@ public class Workshop {
 
     // Método que invierte un arreglo
     public int[] invertirArreglo(int[] arreglo) {
-        // TODO: Implementar el método para invertir un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser [5, 4, 3, 2, 1].
-        return new int[0];
+        int[] invertido = new int [arreglo.lenght];
+	for (int i = 0; i < arreglo.lenght; i++){
+	invertido [i] = arreglo [arreglo.lengght -1 -i];
+       }
+	return invertido;
     }
 
     // Método que ordena un arreglo en orden ascendente
